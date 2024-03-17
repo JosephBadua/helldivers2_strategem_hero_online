e1814c27d4bb0aa8aebfd5db2ca96aadc45db1e0
Joseph Badua <josephbadua1006@gmai.com>
2024-03-17 00:16:38 -0700

    added arrow keys and completion of strategem code

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var score = 0;
 var strategem_name = [];
 var strategem_img = [];
 var strategem_input = [];
-
+var tracking_current = 0;
 
 var strategems = {
     hellbomb: {
@@ -38,6 +38,12 @@ var strategems = {
     }
 }
 
+window.addEventListener('click', function(e){
+       if(new_round == 0){
+            new_game();
+            new_round++;
+        }
+}); 
 
 window.addEventListener('keydown', function(e){
     var keycode = e.keyCode;
@@ -45,13 +51,7 @@ window.addEventListener('keydown', function(e){
        const audio = document.querySelector('#sound');
        audio.currentTime = 0; //rewind to start
        console.log(new_round);
-       if(new_round == 0){
-            new_game();
-            new_round++;
-        } else {
-            play_game();
-        }
-       
+       play_game(keycode);
        audio.play();
     }
 });     
@@ -81,11 +81,6 @@ function new_game(){
            }
         }
 
-        //populate images
-        // console.log(strategem_name);
-        // console.log(strategem_input);
-        // console.log(strategem_img);
-
         for(x = 0; x < strategem_img.length; x++){
             var img = document.getElementById(x + "-image")
             img.src = strategem_img[x];
@@ -94,32 +89,102 @@ function new_game(){
         //populate name
         var name = document.getElementById("strategem-name");
         name.innerHTML = strategem_name[0];
-      
+
         //populate input
-        var input = document.getElementById('inputs');
+        let input_section = document.getElementById('input-section');
 
-
+     
+        for(x = 0; x < strategem_input[0].length; x++){
+             // Create image element
+            let dynamicImage = document.createElement('img');
+            dynamicImage.style.height = "150px";
+            dynamicImage.style.margin = "10px";
+            dynamicImage.style.filter = "brightness(0) saturate(100%) invert(100%) sepia(100%) saturate(0%) hue-rotate(20deg) brightness(103%) contrast(101%)";
+            dynamicImage.setAttribute('data-count', x);
+            if(strategem_input[0][x] == 37){
+                dynamicImage.src = "/left-arrow-button-icon.svg";
+            } else if(strategem_input[0][x]  == 38){
+                dynamicImage.src = "/up-arrow-button-icon.svg";
+            } else if(strategem_input[0][x]  == 39){
+                dynamicImage.src = "/right-arrow-button-icon.svg";
+            } else if(strategem_input[0][x]  == 40){
+                dynamicImage.src = "/down-arrow-button-icon.svg";
+            } 
+       
+            // Add image to DOM
+            input_section.appendChild(dynamicImage);
+        }
 
         //show elements
         document.getElementById("row2-2").classList.remove("hide");
         document.getElementById("row2-3").classList.remove("hide");
         document.getElementById("score-text").classList.remove("hide");
-        document.getElementById("inputs").classList.remove("hide");
+        document.getElementById("input-section").classList.remove("hide");
 
-        document.getElementById("title-text").classList.add("hide");
+        //hide main menu text
         document.getElementById("instruction-text").classList.add("hide");
         document.getElementById("play-text").classList.add("hide");
 }
 
-function play_game(){ 
+function play_game(keycode){ 
         //middle of game, check to see if keydown matches input needed
-        console.log('hello');
-        console.log(strategem_name);
-        console.log(strategem_input);
-        console.log(strategem_img);
+
+        current_strategem = strategem_input[0];
+        console.log(current_strategem);
+        //always will be checking the first keycode of the first array
+        if(keycode == current_strategem[0]){
+
+            var arrow = document.querySelector(`img[data-count="${tracking_current}"]`);
+            arrow.style.filter = "brightness(0) saturate(100%) invert(94%) sepia(93%) saturate(631%) hue-rotate(325deg) brightness(115%) contrast(97%)";
+            current_strategem.shift(); //remove first item
+            
+          //  console.log('What is left ' + current_strategem);
+            tracking_current++;
+        }
+        if(current_strategem.length == 0){
+            //cut first strategem off the list and move on to the second one
+            console.log(strategem_input)
+            strategem_input.shift();
+            check_end(strategem_input);
+        }
+        
 }
 
-function check_end(){
+function check_end(strategem_input){
+    console.log(strategem_input)
 
+    //select all images, remove first one, adjust next image to be focus
+    var images = document.querySelectorAll('.carousel-strategem');
+
+    var elements = document.getElementsByClassName('className');
+    var requiredElement = elements[0];
+
+    console.log(images);
 }
 
+function next_strategem(){
+    
+}
+
+/*
+for(x = 0; x < strategem_input[0].length; x++){
+             // Create image element
+            let dynamicImage = document.createElement('img');
+            dynamicImage.style.height = "150px";
+            dynamicImage.style.margin = "10px";
+            dynamicImage.style.filter = "brightness(0) saturate(100%) invert(100%) sepia(100%) saturate(0%) hue-rotate(20deg) brightness(103%) contrast(101%)";
+            dynamicImage.setAttribute('data-count', x);
+            if(strategem_input[0][x] == 37){
+                dynamicImage.src = "/left-arrow-button-icon.svg";
+            } else if(strategem_input[0][x]  == 38){
+                dynamicImage.src = "/up-arrow-button-icon.svg";
+            } else if(strategem_input[0][x]  == 39){
+                dynamicImage.src = "/right-arrow-button-icon.svg";
+            } else if(strategem_input[0][x]  == 40){
+                dynamicImage.src = "/down-arrow-button-icon.svg";
+            } 
+       
+            // Add image to DOM
+            input_section.appendChild(dynamicImage);
+        }
+*/ 
